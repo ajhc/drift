bdf23e8fde63422f8f776cec692203d8b140cbdb
John Meacham <john@repetae.net>
2007-09-13 12:12:26 +0900

    add rules for deriving Foldable,Functor, and Traversable
    darcs-hash:20070913031226-1a7c6-6ebb67e4032888bb542363a9db5d59bf585fd576.gz

--- src/Rules/FunctorM.hs
+++ src/Rules/FunctorM.hs
@@ -6,71 +6,63 @@ import RuleUtils
 
 rules = [
     ("FunctorM", userRuleFunctorM, "Generics", "derive reasonable fmapM implementation", Nothing),
+    ("Functor", userRuleFunctor, "Generics", "derive reasonable Functor instance", Nothing),
+    ("Foldable", userRuleFoldable, "Generics", "derive instance for Data.Foldable", Nothing),
+    ("Traversable", userRuleTraversable, "Generics", "derive instance for Data.Traversable", Nothing),
     ("RMapM", userRuleRMapM, "Generics", "derive reasonable rmapM implementation", Nothing)
     ]
 
-{- datatype that rules manipulate :-
 
+hasVar tt t = hasType (Var tt) t
 
-data Data = D {	name :: Name,			 -- type's name
-			constraints :: [(Class,Var)],
-			vars :: [Var],		 -- Parameters
-			body :: [Body],
-			derives :: [Class],	 -- derived classes
-			statement :: Statement}  -- type of statement
-	   | Directive				 --|
-	   | TypeName Name			 --| used by derive (ignore)
-		deriving (Eq,Show)
-
-data Body = Body { constructor :: Constructor,
-		    labels :: [Name], -- [] for a non-record datatype.
-		    types :: [Type]} deriving (Eq,Show)
-
-data Statement = DataStmt | NewTypeStmt deriving (Eq,Show)
-
-type Name = String
-type Var = String
-type Class = String
-type Constructor = String
-
-type Rule = (Tag, Data->Doc)
-
--}
-
-{-
-
--- useful helper things
-namesupply   = [text [x,y] | x <- ['a' .. 'z'],
-                             y <- ['a' .. 'z'] ++ ['A' .. 'Z']]
-mknss []     _  = []
-mknss (c:cs) ns =
-  let (thisns,rest) = splitAt (length (types c)) ns
-  in thisns: mknss cs rest
-
-mkpattern :: Constructor -> [a] -> [Doc] -> Doc
-mkpattern c l ns =
-  if null l then text c
-  else parens (hsep (text c : take (length l) ns))
-
-instanceheader cls dat =
-  let fv     = vars dat
-      tycon  = name dat
-      ctx    = map (\v-> text cls <+> text v)
-      parenSpace = parens . hcat . sepWith space
-  in
-  hsep [ text "instance"
-       , opt fv (\v -> parenList (ctx v) <+> text "=>")
-       , text cls
-       , opt1 (texts (tycon: fv)) parenSpace id
-       , text "where"
-       ]
-
--}
+hasType tt t = has t where
+    has t | t == tt = True
+    has (List t) = has t
+    has (Arrow a b) = has a || has b
+    has (LApply t ts) = any has (t:ts)
+    has (Tuple ts) = any has (ts)
+    has _ = False
 
 
 
--- begin here for Binary derivation
+userRuleFoldable D{name = name, vars = [] } = text "--" <+> text name <> text ": Cannot derive Foldable without type variables"
+userRuleFoldable D{name = name, vars = vars, body=body } = ins where
+    (tt:rt') = reverse vars
+    rt = reverse rt'
+    fn = if null rt then text name else parens (text name <+> hsep (map text rt))
+    ins = text "instance" <+> text "Foldable" <+> fn <+> text "where" $$ block fs
+    fs = map f' $ body
+    combine xs = if null xs then text "Data.Monoid.mempty" else hsep (intersperse (text "`Data.Monoid.mappend`") xs)
+    f' Body{constructor=constructor, types=types} = text "foldMap" <+> f <+> pattern constructor types <+> equals <+>  combine (concatMap g (zip types vnt)) where
+        vnt = varNames types
+        g (t,n) | not (hasVar tt t) = []
+        g (Var t,n) | t == tt = [parens $ f <+> n]
+        g (List (Var t),n) | t == tt =  [parens $ text "Data.Monoid.mconcat $ Prelude.map" <+> f <+> n]
+        g (List t,n)  = [parens $ text "Data.Monoid.mconcat $ Prelude.map" <+> lf t <+> n]  where
+            lf t = parens $ text "\\x ->" <+> combine (g (t,x))
+        g (LApply t [],n) = g (t,n)
+        g (LApply t ts,n)  = [parens $ text "Data.Foldable.foldMap" <+> f <+> n]
+        g (Tuple ts,n) = [parens $ text "case" <+> n <+> text "of" <+> tuple (varNames ts) <+> rArrow <+> combine (concatMap g (zip ts (varNames ts)))]
+        g _ = []
 
+userRuleFunctor D{name = name, vars = [] } = text "--" <+> text name <> text ": Cannot derive Functor without type variables"
+userRuleFunctor D{name = name, vars = vars, body=body } = ins where
+    (tt:rt') = reverse vars
+    rt = reverse rt'
+    fn = if null rt then text name else parens (text name <+> hsep (map text rt))
+    ins = text "instance" <+> text "Functor" <+> fn <+> text "where" $$ block fs
+    fs = map f' $ body
+    f' Body{constructor=constructor, types=types} = text "fmap" <+> text "f" <+> pattern constructor types <+> equals <+> text constructor <+>  hsep (map g (zip types vnt)) where
+        vnt = varNames types
+        g (t,n) | not (hasVar tt t) = n
+        g (Var t,n) | t == tt = parens $ f <+> n
+        g (List (Var t),n) | t == tt =  parens $ text "Prelude.map" <+> f <+> n
+        g (List t,n)  = parens $ text "Prelude.map" <+> lf t <+> n  where
+            lf t = parens $ text "\\x ->" <+> g (t,x)
+        g (LApply t [],n) = g (t,n)
+        g (LApply t ts,n) | last ts == Var tt = parens $ text "fmap" <+> f <+> n
+        g (Tuple ts,n) = parens $ text "case" <+> n <+> text "of" <+> tuple (varNames ts) <+> rArrow <+> tuple (map g (zip ts (varNames ts)))
+        g _ = empty
 
 userRuleFunctorM D{name = name, vars = [] } = text "--" <+> text name <> text ": Cannot derive FunctorM without type variables"
 userRuleFunctorM D{name = name, vars = vars, body=body } = ins where
@@ -81,7 +73,7 @@ userRuleFunctorM D{name = name, vars = vars, body=body } = ins where
     fs = map f' $ body
     f' Body{constructor=constructor, types=types} = text "fmapM" <+> text "f" <+> pattern constructor types <+> equals <+> text "do" <+> hcat (map g (zip types vnt)) <+> text "return $" <+> text constructor <+> hsep vnt where
         vnt = varNames types
-        g (t,n) | not (has t) = empty
+        g (t,n) | not (hasVar tt t) = empty
         g (Var t,n) | t == tt = n <+> lArrow <+> text "f" <+> n <> semicolon
         g (List (Var t),n) | t == tt = n <+> lArrow <+> text "mapM" <+> f <+> n <> semicolon
         g (List t,n)  = n <+> lArrow <+> text "mapM" <+> lf t <+> n <> semicolon  where
@@ -90,12 +82,6 @@ userRuleFunctorM D{name = name, vars = vars, body=body } = ins where
         g (LApply t ts,n) | last ts == Var tt = n <+> lArrow <+> text "fmapM" <+> f <+> n <> semicolon
         g (Tuple ts,n) = n <+> lArrow <+> (parens $ text "do" <+> tuple (varNames ts) <+> lArrow <+> text "return" <+> n <> semicolon  <+> hcat (map g (zip ts (varNames ts))) <> text "return" <+> tuple (varNames ts)) <> semicolon
         g _ = empty
-    has (Var t) | t == tt = True
-    has (List t) = has t
-    has (Arrow a b) = has a || has b
-    has (LApply t ts) = any has (t:ts)
-    has (Tuple ts) = any has (ts)
-    has _ = False
 
 userRuleRMapM D{name = name, vars = vars, body=body } = ins where
     --(tt:rt') = reverse vars
@@ -106,7 +92,7 @@ userRuleRMapM D{name = name, vars = vars, body=body } = ins where
     fs = map f' $ body
     f' Body{constructor=constructor, types=types} = text "rmapM" <+> text "f" <+> pattern constructor types <+> equals <+> text "do" <+> hcat (map g (zip types vnt)) <+> text "return $" <+> text constructor <+> hsep vnt where
         vnt = varNames types
-        g (t,n) | not (has t) = empty
+        g (t,n) | not (hasType tt t) = empty
         g ( t,n) | t == tt = n <+> lArrow <+> text "f" <+> n <> semicolon
         g (List (t),n) | t == tt = n <+> lArrow <+> text "mapM" <+> f <+> n <> semicolon
         g (List t,n)  = n <+> lArrow <+> text "mapM" <+> lf t <+> n <> semicolon  where
@@ -115,10 +101,26 @@ userRuleRMapM D{name = name, vars = vars, body=body } = ins where
         g (LApply t ts,n) | last ts ==  tt = n <+> lArrow <+> text "fmapM" <+> f <+> n <> semicolon
         g (Tuple ts,n) = n <+> lArrow <+> (parens $ text "do" <+> tuple (varNames ts) <+> lArrow <+> text "return" <+> n <> semicolon  <+> hcat (map g (zip ts (varNames ts))) <> text "return" <+> tuple (varNames ts)) <> semicolon
         g _ = empty
-    has t | t == tt = True
-    has (List t) = has t
-    has (Arrow a b) = has a || has b
-    has (LApply t ts) = any has (t:ts)
-    has (Tuple ts) = any has (ts)
-    has _ = False
+
+userRuleTraversable D{name = name, vars = [] } = text "--" <+> text name <> text ": Cannot derive Traversable without type variables"
+userRuleTraversable D{name = name, vars = vars, body=body } = ins where
+    (tt:rt') = reverse vars
+    rt = reverse rt'
+    fn = if null rt then text name else parens (text name <+> hsep (map text rt))
+    ins = text "instance" <+> text "Data.Traversable.Traversable" <+> fn <+> text "where" $$ block fs
+    fs = map f' $ body
+    combine xs = if null xs then empty else text "<$>" <+> hsep (intersperse (text "<*>") xs)
+    f' Body{constructor=constructor, types=types} = text "traverse" <+> f <+> pattern constructor types <+> equals <+> text constructor <+> combine (map g (zip types vnt)) where
+        vnt = varNames types
+        g (t,n) | not (hasVar tt t) = text "Control.Applicative.pure" <+> n
+        g (Var t,n) | t == tt = f <+> n
+        g (List (Var t),n) | t == tt = text "traverse" <+> f <+> n
+        g (List t,n)  = text "traverse" <+> lf t <+> n  where
+            lf t = parens $ text "\\x ->" <+> g (t,x)
+        g (LApply t [],n) = g (t,n)
+        g (LApply t ts,n) | last ts == Var tt = text "traverse" <+> f <+> n
+--        g (Tuple ts,n) = (parens $  tuple (varNames ts) <+> lArrow <+> text "return" <+> n <> semicolon  <+> hcat (map g (zip ts (varNames ts))) <> text "return" <+> tuple (varNames ts)) <> semicolon
+        g (Tuple ts,n) = parens $ text "case" <+> n <+> text "of" <+> tuple (varNames ts) <+> rArrow <+> text ("(" ++ replicate (length ts - 1) ',' ++ ")") <+> combine (map g (zip ts (varNames ts)))
+        g _ = empty
+
 
